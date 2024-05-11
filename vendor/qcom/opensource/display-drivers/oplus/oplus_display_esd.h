182dc27e510d2bc382d26b832bf9257ebee9e19c
pswbuild <pswbuild@oppo.com>
2024-05-11 10:48:01 +0800

    Synchronize code for OPPO PJR110_14.0.1.415(CN01) Based on QCOM release TAG: platform/vendor/qcom-proprietary/ese-devicetree:AU_LINUX_ANDROID_LA.VENDOR.13.2.0.R1.11.00.00.925.084

---
+++ vendor/qcom/opensource/display-drivers/oplus/oplus_display_esd.h
@@ -0,0 +1,22 @@
+/***************************************************************
+** Copyright (C), 2022, OPLUS Mobile Comm Corp., Ltd
+**
+** File : oplus_display_esd.h
+** Description : oplus esd feature
+** Version : 2.0
+** Date : 2021/11/26
+** Author : Display
+******************************************************************/
+#ifndef _OPLUS_ESD_H_
+#define _OPLUS_ESD_H_
+
+#include "dsi_panel.h"
+#include "dsi_defs.h"
+#include "oplus_display_private_api.h"
+
+int oplus_panel_parse_esd_reg_read_configs(struct dsi_panel *panel);
+bool oplus_panel_validate_reg_read(struct dsi_panel *panel);
+void oplus_panel_esd_set_page(struct dsi_panel *panel, int cmd_index);
+
+#endif /* _OPLUS_ESD_H_ */
+
---
+++ vendor/qcom/proprietary/camera-devicetree/config/kalama.mk
@@ -0,0 +1,34 @@
+dtbo-$(CONFIG_ARCH_KALAMA) := kalama-camera.dtbo
+
+#dtbo-$(CONFIG_ARCH_KALAMA) += kalama-camera-sensor-cdp.dtbo \
+#								kalama-camera-sensor-mtp.dtbo \
+#								kalama-camera-sensor-qrd.dtbo \
+#								kalama-camera-sensor-hdk.dtbo \
+#								kalama-sg-hhg-camera.dtbo \
+#								kalama-sg-hhg-camera-sensor.dtbo
+
+#PLUS_DTS_OVERLAY start
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/wukong-camera-overlay.dtbo \
+
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/salami-camera-overlay.dtbo \
+
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/xueying-camera-overlay.dtbo \
+
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/zonda-camera-overlay.dtbo \
+
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/xigua-camera-overlay.dtbo  \
+
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/aston-camera-overlay.dtbo  \
+
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/aston-camera-overlay-T0.dtbo  \
+
+dtbo-$(CONFIG_ARCH_KALAMA) += oplus/astondomastic-camera-overlay-T0.dtbo  \
+
+#OPLUS_DTS_OVERLAY end
+
+# dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
+# 				crow-camera-sensor-idp.dtbo \
+# 				crow-camera-sensor-qrd.dtbo
+
+dtbo-$(CONFIG_ARCH_CROW) += crow-camera.dtbo \
+				oplus/benz-camera-overlay.dtbo
